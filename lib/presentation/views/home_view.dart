3db71af1fd3bb740f527a3caa76edcbbc75f7a0c
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2024-11-24 18:04:47 +0200

    new commit with app file

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -2,14 +2,24 @@ import 'package:flutter/material.dart';
 
 import '../widgets/ahmed_sec_one.dart';
 import '../widgets/karim_sec_one.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const  Scaffold(
       body: Column(
         children: [
+          Text("Home View"),
+          SizedBox(
+            height: 20,
+          ),
+          Card(
+            color: Colors.amber,
+            elevation: 2,
+            child: Center(child: Text("Ahmed Sec One")),
+          ),
           AhmedSecOne(),
           KarimSecOne(),
         ],
