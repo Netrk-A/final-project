b6f770fc61c3ac893e8cd80d19fff0a0ca912c07
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2024-11-24 18:04:49 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+// edited by ahmed abduallah
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
