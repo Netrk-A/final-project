ab1ec5eb0dbaf2dfcd304dd4731308fcedb9c63a
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2024-11-25 20:50:23 +0200

    finish category section (make category data form api )

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:peakmart/features/home/presentation/views/category_section/catrgory_view.dart';
 import 'package:peakmart/features/home/presentation/views/landing_section/landing_view.dart';
+import 'package:peakmart/features/home/presentation/views/offers_section/offers_view.dart';
 
-class HomeView  extends StatelessWidget {
-  const HomeView ({super.key});
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         LandingView(),
+
+
+        OfferSView(),
+        CategorySection(),
       ],
     );
   }
